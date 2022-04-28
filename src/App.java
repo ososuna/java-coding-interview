66a3a816b0dda8091ee4cf2bdc00480dc21da03e
oswi <oswaldo.adrian@live.com.mx>
2022-04-28 00:16:27 -0500

    multiply

--- src/App.java
+++ src/App.java
@@ -1,5 +1,7 @@
+import classes.Multiply;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    }
+	public static void main(String[] args) throws Exception {
+		System.out.println(Multiply.multiply(-3, 5));
+	}
 }
