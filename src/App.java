b473da794731bf877a8448111242675fce27416b
oswi <oswaldo.adrian@live.com.mx>
2022-07-15 12:37:26 -0600

    improve remove duplicates

--- src/App.java
+++ src/App.java
@@ -2,8 +2,8 @@ import classes.Exercises;
 
 public class App {
 	public static void main(String[] args) throws Exception {
-		int[] arr = {3, 9, 2, 3, 1, 7, 2, 3, 5}; // key = 3, output = [9, 2, 1, 7, 2, 5]
-		int[] result = Exercises.removeAllOccurrences(arr, 3);
+		int[] arr = {1, 1, 2, 3, 3, 3, 3, 4, 5};
+		int[] result = Exercises.removeDuplicatesFromSortedArray(arr);
 		for (int i = 0; i < result.length; i++) {
 			System.out.print(result[i] + " ");
 		}
--- src/classes/Exercises.java
+++ src/classes/Exercises.java
@@ -155,21 +155,20 @@ public class Exercises {
     }
   }
   
-  public static int removeDuplicates(int[] arr) {
+  public static int[] removeDuplicatesFromSortedArray(int[] arr) {
     if (arr.length == 0 || arr.length == 1)
-      return arr.length;
+      return arr;
     int j = 0;
     for (int i = 0; i < arr.length-1; i++)
       if (arr[i]!=arr[i+1])
         arr[j++] = arr[i];
       arr[j++] = arr[arr.length-1];
-    return j;
+    return Arrays.copyOf(arr, j);
   }
 
   public static int[] removeAllOccurrences(int[] arr, int k) {
-    if (arr.length == 0 || arr.length == 1) {
+    if (arr.length == 0 || arr.length == 1)
       return arr;
-    }
     int n = 0;
     for (int i = 0; i < arr.length; i++)
       if (arr[i] != k)
