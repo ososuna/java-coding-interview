5e3bece81e690ad47c7a26a44137465fcb613a61
ososuna <oswaldo.adrian@live.com.mx>
2023-02-07 21:37:33 -0600

    create maven project

---
+++ src/main/java/dev/ososuna/App.java
@@ -0,0 +1,13 @@
+package dev.ososuna;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
