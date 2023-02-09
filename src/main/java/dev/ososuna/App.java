0083435e31cfc8057d00880ed2cf26fec1f0067f
oswi <oswaldo.adrian@live.com.mx>
2023-02-08 18:24:41 -0600

    reindent lines

--- src/main/java/dev/ososuna/App.java
+++ src/main/java/dev/ososuna/App.java
@@ -1,13 +1,6 @@
 package dev.ososuna;
-
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+  }
 }
